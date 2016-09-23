daa852c70de5963842b83391a6a4d35643380a60
Nabina Shahi <nabina.shahi@gmail.com>
2016-09-23 11:23:16 -0500

    test

--- src/main/java/org/shopping/app/HomeController.java
+++ src/main/java/org/shopping/app/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home Nabina shahi! The client locale is {}.", locale);
+		logger.info("Welcome home Nabina shahi test! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
